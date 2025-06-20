d81bc25c424bca2c94299262e3a364a857444e2c
Ethan <116187182+Cubeacabra@users.noreply.github.com>
2025-06-20 16:47:46 -0700

    base html/css/js
    no true functionality yet but the base for some stuff is there

---
+++ game.js
@@ -0,0 +1,11 @@
+let gameBox = document.getElementById("gameBox");
+let playerElement = document.createElement("div");
+playerElement.setAttribute("class", "playerElemenet");
+
+class Player {
+	constructor(e) {
+		this.e = e;
+	}
+}
+
+function runGame() {}
