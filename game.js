f9f0081ca767dd2385edbd090664b049a5ee4dc3
Ethan <116187182+Cubeacabra@users.noreply.github.com>
2025-08-03 22:05:37 -0700

    Obstacles, gravity, basic jumping
    Added the obstacles using CSS keyframes, gravity with the player class and set interval, and a jump in the player class. jumping is way too choppy and needs to be fixed

--- game.js
+++ game.js
@@ -1,17 +1,54 @@
 let gameBox = document.getElementById("gameBox");
-let playerElement = document.createElement("div");
-playerElement.setAttribute("class", "playerElemenet");
+let playerElement = document.getElementById("playerElement");
+let hole = document.getElementById("hole");
 
 class Player {
 	constructor(e) {
 		this.e = e;
-		this.x = 100;
-		this.y = 100;
-		this.dy = 0;
+		this.y = parseInt(getComputedStyle(e).top);
+		this.dy = 1;
+		this.d2y = 1;
+		this.jumps = 0;
+		this.jumping = false;
 	}
 	jump() {
-		this.y += 100;
+		this.jumping = true;
+		while (this.jumps <= 100) {
+			this.y -= this.dy;
+			this.jumps++;
+		}
+		this.e.style.top = this.y + "px";
+
+		this.jumps = 0;
+		this.jumping = false;
+	}
+	gravity() {
+		this.y += this.dy;
+		this.e.style.top = this.y + "px";
 	}
 }
 
-function runGame() {}
+hole.addEventListener("animationiteration", () => {
+	hole.style.top = -(Math.random() * 73 + 27) + "%";
+});
+
+let character = new Player(playerElement);
+
+setInterval(
+	(obj) => {
+		if (!obj.jumping && obj.y < 484) {
+			let top = obj.y + 3;
+			obj.y = top;
+			if (obj.y >= 484) {
+				obj.y = 484;
+			}
+			obj.gravity();
+		}
+	},
+	10,
+	character
+);
+
+document.addEventListener("click", () => {
+	character.jump();
+});
